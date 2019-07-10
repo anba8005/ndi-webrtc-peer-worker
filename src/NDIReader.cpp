e2a8bf14374bfdd260a88a2274689ee38bb175e2
Andrejus <anba8005@gmail.com>
2019-07-10 23:09:05 +0300

    small fix

--- src/NDIReader.cpp
+++ src/NDIReader.cpp
@@ -78,7 +78,7 @@ void NDIReader::open(Configuration config) {
         NDIlib_recv_create_v3_t recv_create_desc;
         recv_create_desc.source_to_connect_to = _ndi_source;
         recv_create_desc.bandwidth = NDIlib_recv_bandwidth_highest;
-        recv_create_desc.color_format = NDIlib_recv_color_format_UYVY_RGBA;
+        recv_create_desc.color_format = NDIlib_recv_color_format_fastest;
         recv_create_desc.p_ndi_recv_name = "NDIReader";
         recv_create_desc.allow_video_fields = false;
 
