dd34e14bcd412ddb13c67fbb3fc3d7abee1c4c4d
anba8005 <anba8005@gmail.com>
2019-10-18 16:29:24 +0300

    small fix :)

--- scripts/docker-build-linux.sh
+++ scripts/docker-build-linux.sh
@@ -1,3 +1,3 @@
 docker run -v $PWD/../src:/code/src -v $PWD/../bin:/code/build/bin \
 	-v $PWD/../build:/code/build/CMakeFiles/ndi-webrtc-peer-worker.dir/src \
-	--rm -it ndi-worker-build-image-linux:latest $1
+	--rm -it ndi-worker-build-image-linux:latest "$@"
